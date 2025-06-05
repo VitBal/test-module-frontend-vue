839fd07ab0ea6270a17adbeb3878ba01e7d81c37
Mindoza <serg-214-sas@mail.ru>
2025-06-05 12:59:00 +0500

    add interceptors and btns

--- src/views/ErrorNotFound.vue
+++ src/views/ErrorNotFound.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="h-screen w-screen flex flex-col justify-center items-center gap-10 bg-linear-to-t from-gray-400 to-white"
+  <div class="h-screen w-screen flex flex-col justify-center items-center gap-10 bg-linear-to-t from-gray-300 to-white"
   >
-    <span class="text-[11rem] font-bold">ЧØЧ</span>
+    <span class="text-[11rem] font-extrabold">ЧØЧ</span>
 
-    <span class="text-[3rem]">Страница не найдена</span>
+    <span class="text-[3rem] font-semibold">Страница не найдена</span>
 
     <button
         type="button"
-        class="uppercase border-black border-1 rounded-lg py-1 px-2 font-bold hover:bg-gray-400"
+        class="uppercase border-black border-2 rounded-lg py-1 px-2 font-extrabold hover:bg-gray-400"
         @click="router.push('/')">
       На главную
     </button>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,78 @@
 <template>
-  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8 gap-3">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <h2
-        class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900"
+          class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900"
       >
         Home
       </h2>
     </div>
 
     <div>
-      <p>authStore: {{ authStore }}</p>
-      <p>userStore: {{ userStore }}</p>
+      <p> <b> authStore: </b>  {{ authStore }}</p>
+    </div>
+    <div>
+      <p> <b> authStore: </b>  {{ userStore.user }}</p>
     </div>
 
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm flex flex-col gap-1">
-      <button
-        type="submit"
-        @click="handleLogin"
-        :class="btnStyle"
-      >
-        Login
-      </button>
-      <button
-          type="submit"
-          @click="handleLogout"
-          :class="btnStyle"
-      >
-        Logout
-      </button>
+    <div class="sm:mx-auto w-[60%] flex flex-wrap gap-1 pt-10">
+      <button type="submit" @click="handleLogin" :class="btnStyle"> Login</button>
+      <button type="submit" @click="handleLogout" :class="btnStyle"> Logout</button>
+      <button type="submit" @click="handle301" :class="btnStyle"> 301</button>
+      <button type="submit" @click="handle403" :class="btnStyle"> 403</button>
+      <button type="submit" @click="handle404client" :class="btnStyle"> 404</button>
+      <button type="submit" @click="handle404back" :class="btnStyle"> 404 back</button>
+      <button type="submit" @click="handle409" :class="btnStyle"> 409</button>
+      <button type="submit" @click="handle419" :class="btnStyle"> 419</button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { useAuthStore } from "@/stores/auth";
-import { useUserStore } from "@/stores/user";
-import { useRouter } from "vue-router";
+import {useAuthStore} from "@/stores/auth";
+import {useUserStore} from "@/stores/user";
+import {useRouter} from "vue-router";
+import {api} from "@/api/index.js";
 
 const authStore = useAuthStore();
 const userStore = useUserStore();
 const router = useRouter();
 
-const btnStyle = 'flex w-full justify-center rounded-md ' +
-    'bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white ' +
-    'shadow-xs hover:bg-indigo-500 focus-visible:outline-2 ' +
-    'focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+const btnStyle = 'uppercase border-black border-1 rounded-lg py-1 px-2 font-bold hover:bg-gray-400 min-w-[10rem]'
 
 const handleLogout = async () => {
-  await authStore.logout(true);
-  await router.push("/login"); // думаю не лучшее место для этого
+  await authStore.logout(true)
+      .then(() => {
+        router.push("/login");
+      });
 };
 
 const handleLogin = () => {
   router.push("/login");
+};
+
+const handle301 = () => {
+  api.get("/api/test-301")
+};
+
+const handle403 = () => {
+  api.get("/api/test-403")
+};
+
+const handle404client = () => {
+  router.push("/fake");
+};
+
+const handle404back = () => {
+  api.get("/api/test-404")
+};
+
+const handle409 = () => {
+  api.get("/api/test-409")
+};
+
+const handle419 = () => {
+  api.get("/api/test-419")
 };
 </script>
 
