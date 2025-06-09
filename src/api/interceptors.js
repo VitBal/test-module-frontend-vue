ae226d16df1799beb390f505da28fdcab58fed94
Vitaly Balanovsky <balanovsky.vit@gmail.com>
2025-06-09 13:41:46 +0500

    fix: router interceptors

--- src/api/interceptors.js
+++ src/api/interceptors.js
@@ -1,21 +1,25 @@
 import { useAppStore } from "@/stores/app";
 import { useUserStore } from "@/stores/user";
+import router from "@/router/index";
+
+const MSG_OBJ_NOT_FIND = "Объект не найден";
+const MSG_SERVER_ERROR = "Произошла внутренняя ошибка сервера";
 
 export const actionsOnError = {
   301: () => useAppStore().setVersionMismatch(),
   401: () => useUserStore().logout(),
   403: () => useAppStore().setAccessDenied(),
-  404: () => useAppStore().showErrorBar("Объект не найден"),
+  404: () => useAppStore().showErrorBar(MSG_OBJ_NOT_FIND),
   409: (error) => useAppStore().showErrorBar(error.response.data.message),
   419: () => location.reload(),
-  422: (error) =>
-    useAppStore().showErrorBar(error.response.data.message),
+  422: (error) => useAppStore().showErrorBar(error.response.data.message),
   423: () => {
-    // редирект !!
     useUserStore().setModuleAccess(false);
-    // router.push("/locked");
+    router.push("/locked");
   },
-  500: () =>
-    useAppStore().showErrorBar("Произошла внутренняя ошибка сервера"),
-  503: () => useAppStore().setMaintenanceMode(true), // редирект !!
+  500: () => useAppStore().showErrorBar(MSG_SERVER_ERROR),
+  503: () => {
+    useAppStore().setMaintenanceMode(true);
+    router.push("/locked");
+  },
 };
