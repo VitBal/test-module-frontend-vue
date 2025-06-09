4d19f44401a6b9a6f385e5ec77ed42bd624c5888
Mindoza <serg-214-sas@mail.ru>
2025-06-09 14:51:29 +0500

    refactor(lockedPage): code improvement

--- src/views/LockedPage.vue
+++ src/views/LockedPage.vue
@@ -1,39 +1,59 @@
 <template>
-  <div class="h-screen w-screen flex flex-col items-center justify-center gap">
-    <p class="text-[3rem] font-semibold">{{ message }}</p>
-    <p class="font-semibold">Обратитесь к администратору</p>
-
-    <div class="pt-5">
-      <button type="submit" @click="router.push({name: 'home'})" :class="btnStyle">На главную</button>
+  <div
+      class="h-screen w-screen min-w-[600px] flex flex-col justify-center text-center items-center gap-5 bg-linear-to-t from-gray-300 to-white">
+    <div class="h-[10rem] w-[10rem]">
+      <img v-if="!userStore.moduleAccess" src="/public/no-access.svg" alt="gear">
+      <img v-if="appStore.maintenanceMode" class="gear" src="/public/gear.svg" alt="gear">
     </div>
-    <div class="pt-5">
+    <div class="text-[3rem] font-extrabold px-2">
+      {{ message.title }}
+    </div>
+    <div class="text-[1rem] font-semibold">
+      {{ message.hint }}
+    </div>
+
+    <div class="flex gap-5">
       <button type="submit" @click="appStore.reload()" :class="btnStyle">Обновить</button>
+
+      <button type="submit" @click="appStore.redirectToMainPage()" :class="btnStyle">На главную</button>
     </div>
   </div>
 </template>
 
 <script setup>
-import {useRouter} from "vue-router";
 import {useUserStore} from "@/stores/user.js";
 import {useAppStore} from "@/stores/app.js";
 import {computed} from "vue";
 
-const router = useRouter();
 const userStore = useUserStore();
 const appStore = useAppStore();
 
 const message = computed(() => {
-  if(!userStore.moduleAccess) {
-    return 'Доступ запрещен'
-  }
-  else if (appStore.maintenanceMode) {
-    return 'Приложение на обслуживании'
-  }
-  else {
-    return 'Как вы тут оказались?'
+  if (!userStore.moduleAccess) {
+    return {title: 'Доступ запрещен', hint: 'Обратитесь к администратору'}
+  } else if (appStore.maintenanceMode) {
+    return {title: 'Приложение в режиме обслуживания', hint: 'Попробуйте зайти позже'}
+  } else {
+    return {title: 'Как вы тут оказались?', hint: '☺'}
   }
 })
 
 const btnStyle =
-    "uppercase border-black border-1 rounded-lg py-1 px-2 font-bold hover:bg-gray-400 min-w-[10rem]";
+    "uppercase border-black border-2 rounded-lg py-1 px-2 font-extrabold hover:bg-gray-400";
 </script>
+
+
+<style scoped>
+@keyframes rotate {
+  from {
+    transform: rotate(0deg)
+  }
+  to {
+    transform: rotate(360deg)
+  }
+}
+
+.gear {
+  animation: rotate 5s linear infinite;
+}
+</style>
