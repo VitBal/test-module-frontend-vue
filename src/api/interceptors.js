223f67355c8fc75329bca7ad509149a4f6e9f7b1
Mindoza <serg-214-sas@mail.ru>
2025-06-09 13:26:58 +0500

    fix: interceptors.js

--- src/api/interceptors.js
+++ src/api/interceptors.js
@@ -3,19 +3,19 @@ import { useUserStore } from "@/stores/user";
 
 export const actionsOnError = {
   301: () => useAppStore().setVersionMismatch(),
-  401: () => useUserStore().logout(false),
+  401: () => useUserStore().logout(),
   403: () => useAppStore().setAccessDenied(),
-  404: () => useAppStore().showErrorBar("Объект не найден", "negative"),
-  409: () => useAppStore().showErrorBar(error.response.data.message, "warning"),
+  404: () => useAppStore().showErrorBar("Объект не найден"),
+  409: (error) => useAppStore().showErrorBar(error.response.data.message),
   419: () => location.reload(),
   422: (error) =>
-    appStore.showErrorBar(error.response.data.message, "negative"),
+    useAppStore().showErrorBar(error.response.data.message),
   423: () => {
     // редирект !!
     useUserStore().setModuleAccess(false);
     // router.push("/locked");
   },
   500: () =>
-    appStore.showErrorBar("Произошла внутренняя ошибка сервера", "negative"),
+    useAppStore().showErrorBar("Произошла внутренняя ошибка сервера"),
   503: () => useAppStore().setMaintenanceMode(true), // редирект !!
 };
